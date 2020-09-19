45eb4fe410a6c9cf1d688674de90e02c6886a55c
amj88804 <71186503+amj88804@users.noreply.github.com>
2020-09-19 13:55:28 -0500

    Scenario #1
    Use pet names, and addd Marmaduke to the end, which then also allows the use to add their name to end.

--- forms/Form1/Form1.js
+++
@@ -1,6 +0,0 @@
-let firstName = prompt(`Enter your first name`)
-let lastName = prompt(`Enter your last name`)
-alert(`Nice to meet you ${firstName} ${lastName}`)
-
-let age = 21
-alert(`Alexandra is 21`)
